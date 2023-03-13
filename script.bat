c1fdff1d278a3a4a23a0991ec8a72d823182df96
Rince Mathew <rincemathewmanattu@gmail.com>
2023-03-13 17:23:30 +0530

    script changes

--- script.bat
+++ script.bat
@@ -1,4 +1,4 @@
-ECHO ON
+ECHO OFF
 REM A batch script to execute a Python script
 @REM SET PATH=%PATH%;C:\Users\91949\Documents\Personal-folder\rince-mathew\
 C:\Users\91949\Documents\Personal-folder\rince-mathew\env\Scripts\activate && python C:\Users\91949\Documents\Personal-folder\rince-mathew\app.py
